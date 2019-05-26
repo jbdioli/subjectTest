ce4846db0d001bf6a04643f29d9c359ab89b00b0
jbdioli <jdeoworld@gmail.com>
2019-05-26 23:22:09 +0200

    add

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 
 import { ArrayModel } from '../models/array.model'
 import { StorageService } from '../services/storage.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 
@@ -15,21 +16,34 @@ import { StorageService } from '../services/storage.service';
 })
 export class HomePage implements OnInit, OnDestroy {
   obsSubscription: Subscription;
+  memoryForm: FormGroup;
 
-  infoData: ArrayModel[]= [];
+  infoData: ArrayModel[] = [];
 
-  constructor(private modalController: ModalController, public navCtrl: NavController, private storageService: StorageService) {}
+  constructor(private modalController: ModalController, public navCtrl: NavController, private storageService: StorageService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.storageService.getData();
     this.obsSubscription = this.storageService.dbSubject.subscribe((value: ArrayModel[]) => {
       this.infoData = value;
+      console.log('From Page.ts : ', this.infoData);
     },
-    (error) => {
-      console.log(error);
-    });
-    console.log('From Page.ts');
-    console.log(this.infoData);
+      (error) => {
+        console.log('Error : ', error);
+      });
+
+
+    // this.memoryForm = this.fb.group({ name: ['', Validators.required] });
+    this.memoryForm = this.fb.group({ name: ['', Validators.required] });
+
+    console.log('Form created : ', this.memoryForm.value);
+  }
+
+  saveMemory() {
+    const nameControl = this.memoryForm.get('name');
+    this.infoData[0].name = nameControl.value;
+    console.log('array :', this.infoData);
+    console.log('memory form : ', this.memoryForm.value);
   }
 
   ngOnDestroy() {
